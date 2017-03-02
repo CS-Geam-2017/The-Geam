e11f36cc46a8cd3732e6c6ed0f0c6fc091b0dcf9
S31539 <S31539@PS-1T1-SL-10750.parkhill.k12.mo.us>
2017-03-02 14:53:04 -0600

    Changing speed of players

--- CS Geam3/src/com/Geam/main/KeyInput.java
+++ CS Geam3/src/com/Geam/main/KeyInput.java
@@ -29,15 +29,15 @@ public class KeyInput extends KeyAdapter {
 			if(tempObject.getID() == ID.Player){
 				//key events for player 1;
 				if(key == KeyEvent.VK_W && upKey==false) {
-					tempObject.setSpeedY(tempObject.getSpeedY()-5);
+					tempObject.setSpeedY(tempObject.getSpeedY()-3);
 					upKey=true;
 				}
 				if(key == KeyEvent.VK_S && downKey==false) {
-					tempObject.setSpeedY(tempObject.getSpeedY()+5);
+					tempObject.setSpeedY(tempObject.getSpeedY()+3);
 					downKey=true;
 				}
 				if(key == KeyEvent.VK_A && leftKey==false) {
-					tempObject.setSpeedX(tempObject.getSpeedX()-5);
+					tempObject.setSpeedX(tempObject.getSpeedX()-3);
 					leftKey=true;
 				}
 				if(key == KeyEvent.VK_D && rightKey==false) {
@@ -52,15 +52,15 @@ public class KeyInput extends KeyAdapter {
 					upKey2=true;
 				}
 				if(key == KeyEvent.VK_DOWN && downKey2==false) {
-					tempObject.setSpeedY(tempObject.getSpeedY()+5);
+					tempObject.setSpeedY(tempObject.getSpeedY()+3);
 					downKey2=true;
 				}
 				if(key == KeyEvent.VK_LEFT && leftKey2==false) {
-					tempObject.setSpeedX(tempObject.getSpeedX()-5);
+					tempObject.setSpeedX(tempObject.getSpeedX()-3);
 					leftKey2=true;
 				}
 				if(key == KeyEvent.VK_RIGHT && rightKey2==false) {
-					tempObject.setSpeedX(tempObject.getSpeedX()+5);
+					tempObject.setSpeedX(tempObject.getSpeedX()+3);
 					rightKey2=true;
 				}
 			}
@@ -76,19 +76,19 @@ public class KeyInput extends KeyAdapter {
 			if(tempObject.getID() == ID.Player2) {
 				//key events for player 2
 				if(key == KeyEvent.VK_LEFT) {
-					tempObject.setSpeedX(tempObject.getSpeedX()+5);
+					tempObject.setSpeedX(tempObject.getSpeedX()+3);
 					leftKey2=false;
 				}
 				if(key == KeyEvent.VK_UP) {
-					tempObject.setSpeedY(tempObject.getSpeedY()+5);
+					tempObject.setSpeedY(tempObject.getSpeedY()+3);
 					upKey2=false;
 				}
 				if(key == KeyEvent.VK_RIGHT) {
-					tempObject.setSpeedX(tempObject.getSpeedX()-5);
+					tempObject.setSpeedX(tempObject.getSpeedX()-3);
 					rightKey2=false;
 				}
 				if(key == KeyEvent.VK_DOWN) {
-					tempObject.setSpeedY(tempObject.getSpeedY()-5);
+					tempObject.setSpeedY(tempObject.getSpeedY()-3);
 					downKey2=false;
 				}
 			}
@@ -96,15 +96,15 @@ public class KeyInput extends KeyAdapter {
 			if(tempObject.getID() == ID.Player) {
 				//key events for player
 				if(key == KeyEvent.VK_W) {
-					tempObject.setSpeedY(tempObject.getSpeedY()+5);
+					tempObject.setSpeedY(tempObject.getSpeedY()+3);
 					upKey=false;
 				}
 				if(key == KeyEvent.VK_S) {
-					tempObject.setSpeedY(tempObject.getSpeedY()-5);
+					tempObject.setSpeedY(tempObject.getSpeedY()-3);
 					downKey=false;
 				}
 				if(key == KeyEvent.VK_A) {
-					tempObject.setSpeedX(tempObject.getSpeedX()+5);
+					tempObject.setSpeedX(tempObject.getSpeedX()+3);
 					leftKey=false;
 				}
 				if(key == KeyEvent.VK_D) {
