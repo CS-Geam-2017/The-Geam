9cb66329e9ac0a26486e72f99484c222d4d8facf
S31539 <S31539@PS-1T1-SL-10750.parkhill.k12.mo.us>
2017-11-29 16:38:33 -0600

    It works

--- CS Geam3/src/com/Geam/main/Geam.java
+++ CS Geam3/src/com/Geam/main/Geam.java
@@ -28,49 +28,180 @@ public class Geam extends Canvas implements Runnable {
 	private Random r;
 	private Handler handler;
 	private int frames;
-	private ArrayList<Point2D> Walls = new ArrayList<Point2D>();
-	public Node start = new Node(new Point2D(101,51),null, 0, getDist(new Point2D(101,51),new Point2D(61,101)), getDist(new Point2D(101,51),new Point2D(61,101))) ;
-	public Node end = new Node(new Point2D(61,101), null, 10000, 0, 10000);
-	public Node cur = start;
-	private ArrayList<Node> openSet = new ArrayList<Node>();
-	private ArrayList<Node> closedSet = new ArrayList<Node>();
+	private static ArrayList<Point2D> Walls = new ArrayList<Point2D>();
+	public static Node start = new Node(new Point2D(101,51),null, 0, getDist(new Point2D(101,51),new Point2D(61,101)), getDist(new Point2D(101,51),new Point2D(61,101))) ;
+	public static Node end = new Node(new Point2D(401,101), null, 10000, 0, 10000);
+	public static Node cur = start;
+	private static ArrayList<Node> openSet = new ArrayList<Node>();
+	private static ArrayList<Point2D> openSetXY = new ArrayList<Point2D>();
+	private static ArrayList<Point2D> closedSet = new ArrayList<Point2D>();
 	
 	public Geam() {
 		this.addKeyListener(new KeyInput(handler));
 		// ^ Tells computer to listen for key inputs
-		
+		//System.out.println(openSet.size());
+		openSet.add(start);
+		closedSet.add(new Point2D(200,200));
+		getSurPoints(start);
+		openSet.remove(0);
+		closedSet.add(start.xy);
 		handler = new Handler();
 		// ^ Needs to know what handler is before game is created
 		new Window(WIDTH, HEIGHT, "Geam", this);
-		
 		r = new Random();
 	}
 	public static double getDist(Point2D a,Point2D b) {
 		return Math.sqrt(Math.pow((Math.abs(a.x-b.x)),2.0) + Math.pow((Math.abs((a.y-b.y))), 2.0));
 	}
 	
-	public ArrayList<Point2D> getSurPoints(Node a){
-		ArrayList<Point2D> list = new ArrayList<Point2D>();
-		for (int i = 0; i<=openSet.size(); i++) {
-			if (openSet.get(i).xy != new Point2D(a.xy.x-10,a.xy.y-10)) {
-				//if (Walls.contains(new Point2D(a.x-10,a.y-10))!= true) openSet.add(new Node(new Point2D(a.x-10,a.y-10), a, i, i, i));
-			}
-			else {
-				if(openSet.get(i).Fcost>(a.Gcost+14+getDist(new Point2D(a.xy.x-10,a.xy.y-10),end.xy))) {
-					openSet.get(i).changeParN(a);
-				}
-			}
-			if (openSet.get(i).xy != a.xy) {
-				if (Walls.contains(new Point2D(a.xy.x,a.xy.y-10))!= true) list.add(new Point2D(a.xy.x,a.xy.y-10));
-			}
-			if (Walls.contains(new Point2D(a.xy.x+10,a.xy.y-10))!= true) list.add(new Point2D(a.xy.x+10,a.xy.y-10));
-			if (Walls.contains(new Point2D(a.xy.x+10,a.xy.y))!= true) list.add(new Point2D(a.xy.x+10,a.xy.y));
-			if (Walls.contains(new Point2D(a.xy.x+10,a.xy.y+10))!= true) list.add(new Point2D(a.xy.x+10,a.xy.y+10));
-			if (Walls.contains(new Point2D(a.xy.x,a.xy.y+10))!= true) list.add(new Point2D(a.xy.x,a.xy.y+10));
-			if (Walls.contains(new Point2D(a.xy.x-10,a.xy.y+10))!= true) list.add(new Point2D(a.xy.x-10,a.xy.y+10));
-			if (Walls.contains(new Point2D(a.xy.x-10,a.xy.y))!= true) list.add(new Point2D(a.xy.x-10,a.xy.y));
+	public static void getSurPoints(Node a){
+		boolean added0 = false;
+		boolean added1 = false;
+		boolean added2 = false;
+		boolean added3 = false;
+		boolean added4 = false;
+		boolean added5 = false;
+		boolean added6 = false;
+		boolean added7 = false;
+		for(int i = 0; i<openSet.size(); i++) {
+			openSetXY.add(openSet.get(i).xy);
 		}
-		return list;
+		for (int i = 0; i<openSet.size(); i++) {
+			//for(int s = 0; s<closedSet.size(); s++) {
+				//System.out.println(s);
+				if (openSetXY.contains(new Point2D(a.xy.x-10,a.xy.y-10)) != true && added0 != true && closedSet.contains(new Point2D(a.xy.x-10,a.xy.y-10)) != true) {
+					//System.out.println(added0);
+					if (Walls.contains(new Point2D(a.xy.x-10,a.xy.y-10))!= true) {
+						added0 = true;
+						//System.out.println(openSet.get(i).xy);
+						//System.out.println(new Point2D(a.xy.x-10,a.xy.y-10));
+						//System.out.println("NOTWORKING0");
+						openSet.add(new Node(new Point2D(a.xy.x-10,a.xy.y-10), a, a.getGcost()+14, getDist(new Point2D(a.xy.x-10,a.xy.y-10), end.xy), (a.getGcost()+14)+getDist(new Point2D(a.xy.x-10,a.xy.y-10), end.xy)));
+					}
+				}
+				else {
+					if(Walls.contains(new Point2D(a.xy.x-10,a.xy.y-10))!= true) {
+						if(openSet.get(i).Fcost>(a.Gcost+14+getDist(new Point2D(a.xy.x-10,a.xy.y-10),end.xy))) {
+							openSet.get(i).changeParN(a);
+						}
+					}
+				}
+				
+				
+				if (openSetXY.contains(new Point2D(a.xy.x,a.xy.y-10)) != true && added1 != true && closedSet.contains(new Point2D(a.xy.x,a.xy.y-10)) != true) {
+					if (Walls.contains(new Point2D(a.xy.x,a.xy.y-10))!= true) {
+						added1 = true;
+						//System.out.println("NOTWORKING1");
+						openSet.add(new Node(new Point2D(a.xy.x,a.xy.y-10), a, a.getGcost()+10, getDist(new Point2D(a.xy.x,a.xy.y-10), end.xy), (a.getGcost()+10)+getDist(new Point2D(a.xy.x,a.xy.y-10), end.xy)));
+					}
+				}
+				else {
+					if(Walls.contains(new Point2D(a.xy.x,a.xy.y-10))!= true) {
+						if(openSet.get(i).Fcost>(a.Gcost+10+getDist(new Point2D(a.xy.x,a.xy.y-10),end.xy))) {
+							openSet.get(i).changeParN(a);
+						}
+					}
+				}
+				
+				
+				if (openSetXY.contains(new Point2D(a.xy.x+10,a.xy.y-10)) != true && added2 != true && closedSet.contains(new Point2D(a.xy.x+10,a.xy.y-10)) != true) {
+					if (Walls.contains(new Point2D(a.xy.x+10,a.xy.y-10))!= true) {
+						added2 = true;
+						//System.out.println("NOTWORKING2");
+						openSet.add(new Node(new Point2D(a.xy.x+10,a.xy.y-10), a, a.getGcost()+14, getDist(new Point2D(a.xy.x+10,a.xy.y-10), end.xy), (a.getGcost()+14)+getDist(new Point2D(a.xy.x+10,a.xy.y-10), end.xy)));
+					}
+				}
+				else {
+					if(Walls.contains(new Point2D(a.xy.x+10,a.xy.y-10))!= true) {
+						if(openSet.get(i).Fcost>(a.Gcost+14+getDist(new Point2D(a.xy.x+10,a.xy.y-10),end.xy))) {
+							openSet.get(i).changeParN(a);
+						}
+					}
+				}
+				
+				
+				if (openSetXY.contains(new Point2D(a.xy.x+10,a.xy.y)) != true && added3 != true && closedSet.contains(new Point2D(a.xy.x+10,a.xy.y)) != true) {
+					if (Walls.contains(new Point2D(a.xy.x+10,a.xy.y))!= true) {
+						added3 = true;
+						//System.out.println("NOTWORKING3");
+						openSet.add(new Node(new Point2D(a.xy.x+10,a.xy.y), a, a.getGcost()+10, getDist(new Point2D(a.xy.x+10,a.xy.y), end.xy), (a.getGcost()+10)+getDist(new Point2D(a.xy.x+10,a.xy.y), end.xy)));
+					}
+				}
+				else {
+					if(Walls.contains(new Point2D(a.xy.x+10,a.xy.y))!= true) {
+						if(openSet.get(i).Fcost>(a.Gcost+10+getDist(new Point2D(a.xy.x+10,a.xy.y),end.xy))) {
+							openSet.get(i).changeParN(a);
+						}
+					}
+				}
+				
+	
+				if (openSetXY.contains(new Point2D(a.xy.x+10,a.xy.y+10)) != true && added4 != true && closedSet.contains(new Point2D(a.xy.x+10,a.xy.y+10)) != true) {
+					if (Walls.contains(new Point2D(a.xy.x+10,a.xy.y+10))!= true) {
+						added4 = true;
+						//System.out.println("NOTWORKING4");
+						openSet.add(new Node(new Point2D(a.xy.x+10,a.xy.y+10), a, a.getGcost()+14, getDist(new Point2D(a.xy.x+10,a.xy.y+10), end.xy), (a.getGcost()+14)+getDist(new Point2D(a.xy.x+10,a.xy.y+10), end.xy)));
+					}
+				}
+				else {
+					if(Walls.contains(new Point2D(a.xy.x+10,a.xy.y+10))!= true) {
+						if(openSet.get(i).Fcost>(a.Gcost+14+getDist(new Point2D(a.xy.x+10,a.xy.y+10),end.xy))) {
+							openSet.get(i).changeParN(a);
+						}
+					}
+				}
+				
+				
+				if (openSetXY.contains(new Point2D(a.xy.x,a.xy.y+10)) != true && added5 != true && closedSet.contains(new Point2D(a.xy.x,a.xy.y+10)) != true) {
+					if (Walls.contains(new Point2D(a.xy.x,a.xy.y+10))!= true) {
+						added5 = true;
+						//System.out.println("NOTWORKING5");
+						openSet.add(new Node(new Point2D(a.xy.x,a.xy.y+10), a, a.getGcost()+10, getDist(new Point2D(a.xy.x,a.xy.y+10), end.xy), (a.getGcost()+10)+getDist(new Point2D(a.xy.x,a.xy.y+10), end.xy)));
+					}
+				}
+				else {
+					if(Walls.contains(new Point2D(a.xy.x,a.xy.y+10))!= true) {
+						if(openSet.get(i).Fcost>(a.Gcost+10+getDist(new Point2D(a.xy.x,a.xy.y+10),end.xy))) {
+							openSet.get(i).changeParN(a);
+						}
+					}
+				}
+				
+				
+				if (openSetXY.contains(new Point2D(a.xy.x-10,a.xy.y+10)) != true && added6 != true && closedSet.contains(new Point2D(a.xy.x-10,a.xy.y+10)) != true) {
+					if (Walls.contains(new Point2D(a.xy.x-10,a.xy.y+10))!= true) {
+						added6 = true;
+						//System.out.println("NOTWORKING6");
+						openSet.add(new Node(new Point2D(a.xy.x-10,a.xy.y+10), a, a.getGcost()+14, getDist(new Point2D(a.xy.x-10,a.xy.y+10), end.xy), (a.getGcost()+14)+getDist(new Point2D(a.xy.x-10,a.xy.y+10), end.xy)));
+					}
+				}
+				else {
+					if(Walls.contains(new Point2D(a.xy.x-10,a.xy.y+10))!= true) {
+						if(openSet.get(i).Fcost>(a.Gcost+14+getDist(new Point2D(a.xy.x-10,a.xy.y+10),end.xy))) {
+							openSet.get(i).changeParN(a);
+						}
+					}
+				}
+				
+				
+				if (openSetXY.contains(new Point2D(a.xy.x-10,a.xy.y)) != true && added7 != true && closedSet.contains(new Point2D(a.xy.x-10,a.xy.y)) != true) {
+					if (Walls.contains(new Point2D(a.xy.x-10,a.xy.y))!= true) {
+						added7 = true;
+						//System.out.println("NOTWORKING7");
+						openSet.add(new Node(new Point2D(a.xy.x-10,a.xy.y), a, a.getGcost()+10, getDist(new Point2D(a.xy.x-10,a.xy.y), end.xy), (a.getGcost()+10)+getDist(new Point2D(a.xy.x-10,a.xy.y), end.xy)));
+					}
+				}
+				else {
+					if(Walls.contains(new Point2D(a.xy.x-10,a.xy.y))!= true) {
+						if(openSet.get(i).Fcost>(a.Gcost+10+getDist(new Point2D(a.xy.x-10,a.xy.y),end.xy))) {
+							openSet.get(i).changeParN(a);
+						}
+					}
+				}
+				//System.out.println(closedSet.size());
+				//System.out.println(openSet.size());
+			//}
+		}
 	}
 
 	
@@ -110,7 +241,7 @@ public class Geam extends Canvas implements Runnable {
 			
 			if(System.currentTimeMillis() - timer > 1000){
 				timer += 1000;
-				System.out.println("FPS:"+frames);
+				//System.out.println("FPS:"+frames);
 				frames = 0;
 			}
 		}
@@ -142,7 +273,13 @@ public class Geam extends Canvas implements Runnable {
 			g.drawLine(i, 0, i, 600);
 		}
 		g.setColor(Color.blue);
-		for (int i = 1; i<=550;i+=10) {
+		for(int i = 1; i<600; i+=10){
+			g.fillRect(i,1, 9, 9);
+			g.fillRect(1, i, 9, 9);
+			Walls.add(new Point2D(i,1));
+			Walls.add(new Point2D(1,i));
+		}
+		for (int i = 1; i<=200;i+=10) {
 			g.fillRect(i, i, 9, 9);
 			g.fillRect(i+10, i, 9, 9);
 			Point2D a = new Point2D(i,i);
@@ -150,15 +287,39 @@ public class Geam extends Canvas implements Runnable {
 			Walls.add(a);
 			Walls.add(b);
 		}
+		Node nextNode = new Node(null, null, 0, 0, 100000000);
+		int nextNodePlace = 0;
+		//System.out.println(openSet.size());
+		g.setColor(Color.magenta);
+		for(int i = 0; i<closedSet.size();i++) {
+			g.fillRect((int)closedSet.get(i).x, (int)closedSet.get(i).y, 9, 9);
+		}
+		g.setColor(Color.cyan);
+		for(int i = 0; i<openSet.size();i++) {
+			//System.out.println(i);
+			g.fillRect((int)openSet.get(i).xy.x, (int)openSet.get(i).xy.y, 9, 9);
+			if (openSet.get(i).Fcost<nextNode.Fcost && closedSet.contains(openSet.get(i).xy)!=true) {
+				nextNode = openSet.get(i);
+				//System.out.println(openSet.get(i));
+				nextNodePlace = i;
+			}
+		}
+		
+		closedSet.add(nextNode.xy);
+		//System.out.println(nextNode.xy);
+		//System.out.println(closedSet.size()-openSet.size());
+		openSet.remove(nextNodePlace);
 		g.setColor(Color.green);
 		g.fillRect((int)start.xy.x, (int)start.xy.y, 9, 9);
 		g.setColor(Color.red);
-		g.setColor(Color.cyan);
-		for(int i = 0; i<=openSet.size();i++) {
-			g.fillRect((int)openSet.get(i).xy.x, (int)openSet.get(i).xy.y, 9, 9);
-		}
-		
 		g.fillRect((int)end.xy.x, (int)end.xy.y, 9, 9);
+		System.out.println(openSet.size());
+		if(!(closedSet.contains(end.xy))) {
+			getSurPoints(nextNode);
+		}
+		else {stop();}
+		g.setColor(Color.black);
+		g.fillRect((int)nextNode.xy.x, (int)nextNode.xy.y, 9, 9);
 		g.dispose();
 		bs.show();
 	}
