c3b9e6fbf947e7af40cc5837f8010904533d38d9
S31539 <S31539@PS-1T1-SL-10750.parkhill.k12.mo.us>
2017-12-06 11:30:57 -0600

    Drawing the person.

--- CS Geam3/src/com/Geam/main/HUD.java
+++ CS Geam3/src/com/Geam/main/HUD.java
@@ -39,5 +39,21 @@ public class HUD {
 			}
 		}
 		g.drawString(display, 500, 300);
+		if(wrongs<=6) {
+			for(int i = 0; i<wrongs; i++) {
+				g.setFont(new Font("Comic Sans MS", Font.BOLD, 40));
+				if (i == 0) g.drawString("O", 100, 100);
+				if (i == 1) g.drawString("|", 106, 130);
+				if (i == 2) g.drawString("\\", 110, 130);
+				if (i == 3) g.drawString("/", 96, 130);
+				if (i == 4) g.drawString("\\", 110, 162);
+				if (i == 5) {
+					g.drawString("/", 96, 162);
+					g.setFont(new Font("Comic Sans MS", Font.BOLD, 10));
+					g.drawString("X", 108, 90);
+					g.drawString("X", 118, 90);
+				}
+			}
+		}
 	}
 }
